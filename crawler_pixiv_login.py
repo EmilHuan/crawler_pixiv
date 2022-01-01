fe363290c1be90c5182263a7b9b8a70f22835767
EmilHuan <s93yoyo695072@gmail.com>
2022-01-01 16:10:30 +0800

    crawler_pixiv_login: 登入版階段性完成 (改為跟非登入版功能相同)

--- crawler_pixiv_login.py
+++ crawler_pixiv_login.py
@@ -34,11 +34,16 @@ executable_path = './chromedriver.exe'
 # 使用 Chrome 的 WebDriver (含 options)
 driver = webdriver.Chrome(options = options)
 
-# 想爬取資料的來源網址
-#url = 'https://www.pixiv.net/users/39495939/bookmarks/artworks' # 我的收藏主頁
+### 想爬取資料的來源網址
 #url = "https://www.pixiv.net/users/13313480/artworks" # 先用「K&P」首頁測試
 #url = "https://www.pixiv.net/users/16481931/artworks"  # 先用「ろあ₍˄·͈༝·͈˄₎」首頁測試
-url = "https://www.pixiv.net/users/2924751/artworks" # 先用「Kaitan」首頁測試
+#url = "https://www.pixiv.net/users/2924751/artworks" # 先用「Kaitan」首頁測試
+#url = "https://www.pixiv.net/users/6657532"
+## 正式使用
+# 我的收藏主頁
+#url = 'https://www.pixiv.net/users/39495939/bookmarks/artworks' 
+# 我的收藏 (公開 -> 未分類)
+url = "https://www.pixiv.net/users/39495939/bookmarks/artworks/%E6%9C%AA%E5%88%86%E9%A1%9E"
 
 # 放置爬取的資料
 listData = []
@@ -96,13 +101,15 @@ def visit():
 
 ## 取得 pixi 主頁每個格子的 url
 def get_url():
+    
+    sleep(3)
     # 取得主要元素的集合
     a_elms = driver.find_elements(By.CSS_SELECTOR, "a.sc-d98f2c-0.sc-rp5asc-16.iUsZyY.sc-bdnxRM.fGjAxR")
 
     # 逐一檢視元素
     for index, a in enumerate(a_elms):
         # 設定要幾張圖片
-        if index >= 10:
+        if index >= 38:
             break
         # 取得圖片連結
         aLink = a.get_attribute("href")
@@ -110,9 +117,6 @@ def get_url():
 
         # 放資料到 list 中
         listLink.append(aLink)
-    
-    print(listLink)
-    print("="*50)
 
 
 ## 取得圖片 url、圖片名稱和作者名稱
@@ -123,33 +127,143 @@ def img_url_name():
     
         sleep(2)
 
-        # 取得圖片網址 (登入前後 CSS 不一樣)
-        imgSrc = driver.find_element(By.CSS_SELECTOR, "div.sc-1qpw8k9-0.gTFqQV a").get_attribute("href")
+        ## 先測試有網頁無動圖 CSS (為了跳過動圖)
+        img_css = driver.find_elements(By.CSS_SELECTOR, "div.sc-tu09d3-1.inGOuX canvas")
+        # 如有代表本連結為動圖，跳過此連結
+        if img_css != []:
+            print("動圖跳過")
+            pass
 
-        # 將圖片網址改為可直接使用的網址，並存到 list (直接用原始網址會顯示 403 error，無法連上圖片)
-        listSrc_useful = imgSrc.replace("i.pximg.net", "i.pixiv.cat")
-    
-        # 取得圖片作者名字 (登入前後 CSS 不一樣)
-        drawer_name = driver.find_element(By.CSS_SELECTOR, "a.sc-d98f2c-0.sc-fujyAs").text
-   
-        # 取得圖片名稱
-        img_name = driver.find_element(By.CSS_SELECTOR, "figcaption.sc-1yvhotl-4.eStCAU h1").text
+        # 如果有，依照爬圖片流程執行
+        else:
+            # 儲存圖片數量的變數 (預設 1 張圖片)
+            img_number = 1
 
-        # 跳轉到圖片網址 (為了獲取圖片網址標題)
-        driver.get(listSrc_useful)
-        # 擷取圖片網址 title 文字 (為了獲取原始圖片解析度)
-        web_page_title = driver.title
-        # 取得圖片解析度 (透過正規表達式從 title 取得)
-        img_resolution = re.search(r"[0-9]+×[0-9]+", web_page_title)[0]       
-    
-        listData.append({
-            "url":link,
-            "author":drawer_name,
-            "img_name":img_name,
-            "img_resolution":img_resolution,
-            "img_use_url":listSrc_useful,
-            "img_origin_url":imgSrc
-        })
+            # 使用 find_elements 將「右上角數字」的元素傳回 list，如果該網頁沒有此元素 (只有一張圖)，會回傳空串列
+            right_number = driver.find_elements(By.CSS_SELECTOR, 'div.sc-zjgqkv-1.cykQFD span')
+        
+            ## 依據網址有無「右上角數字」元素，做不同操作
+            # 如果沒有按鈕 (button 為空串列)，使用原本處理一張圖片的方式
+            if right_number == []:
+                # 取得圖片網址
+                imgSrc = driver.find_element(By.CSS_SELECTOR, "div.sc-1qpw8k9-0.gTFqQV a").get_attribute("href")
+
+                # 將圖片網址改為可直接使用的網址，並存到 list (直接用原始網址會顯示 403 error，無法連上圖片)
+                #imgSrc_useful.append(imgSrc.replace("i.pximg.net", "i.pixiv.cat"))
+                imgSrc_useful = imgSrc.replace("i.pximg.net", "i.pixiv.cat")
+
+                # 取得圖片作者名字 (後綴加入 "> div"，避免擷取到「接搞中」文字)
+                drawer_name = driver.find_element(By.CSS_SELECTOR, "a.sc-d98f2c-0.sc-fujyAs > div").text
+
+                # 取得圖片名稱
+                img_name = driver.find_element(By.CSS_SELECTOR, "figcaption.sc-1yvhotl-4.eStCAU h1").text
+
+                # 跳轉到圖片網址 (為了獲取圖片網址標題)
+                driver.get(imgSrc_useful)
+                # 擷取圖片網址 title 文字 (為了獲取原始圖片解析度)
+                web_page_title = driver.title
+
+                # 取得圖片解析度字串 (透過正規表達式從 title 取得)
+                img_resolution = re.search(r"[0-9]+×[0-9]+", web_page_title)[0]       
+            
+                listData.append({
+                    "web_url":url,
+                    "author":drawer_name,
+                    "img_name":img_name,
+                    "img_number":img_number,
+                    "img_resolution":img_resolution,
+                    "img_use_url":imgSrc_useful,
+                    "img_origin_url":imgSrc
+                })
+
+            # 如果有按鈕 (button 不為空串列)，使用處理多張圖片的方式
+            else:
+                # 取得右上角圖片數字 (e.g. 1/9，代表總共 9 張圖片)
+                right_number = driver.find_element(By.CSS_SELECTOR, 'div.sc-zjgqkv-1.cykQFD span').text
+                # 透過正規表達式取得斜線後面的數字 (注意：此為字串型態)
+                right_number_str = re.search(r"\/([0-9]+)", right_number)[1]
+                # 將取得的數字轉換為整數型態，存到 img_number
+                img_number = int(right_number_str)
+
+                # 點擊「查看全部」按鈕，讓網頁顯示出全部圖片
+                driver.find_element(By.CSS_SELECTOR, 'button[type="button"] div.sc-emr523-2').click()
+
+                # 等待網頁加載
+                sleep(2)
+
+                # 放置每張圖片的原始網址
+                listMulti_imgSrc = []
+                # 放置每張圖片的有效網址
+                listMulti_imgSrc_useful = []
+                # 放置每張圖片的解析度字串
+                listMulti_imgResolution = []
+
+                ## 登入後不知為何不捲動連第 2 張都抓不到；直接改為不判斷張數，全部捲動
+                ## 捲動網頁
+                # 取得每次移動的高度 (使用 js 語法，取得瀏覽器從頭到尾的高度)
+                offset = driver.execute_script(
+                    "return window.document.documentElement.scrollHeight;"
+                )
+
+                # 捲動的 js code
+                # f'' 中，由於嵌入資訊為 {offset}，外側的大括號要寫成 {{}} (跳脫字元)，才不會被視為嵌入資訊  
+                js_code = f'''
+                    window.scrollTo({{
+                        top: {offset},
+                        behavior: "smooth"
+                    }});
+                '''
+
+                # 執行 js code (捲動頁面)
+                driver.execute_script(js_code)
+
+                # 強制等待，讓網頁元素有時間生成
+                sleep(3)
+
+                # 取得所有圖片的網址所在位置的元素 (find_elements)
+                imgSrc_elms = driver.find_elements(By.CSS_SELECTOR, "div.sc-1qpw8k9-0 a")
+
+                # 取得圖片作者名字 (後綴加入 "> div"，避免擷取到「接搞中」文字)
+                drawer_name = driver.find_element(By.CSS_SELECTOR, "a.sc-d98f2c-0.sc-fujyAs > div").text
+
+                # 取得圖片名稱
+                img_name = driver.find_element(By.CSS_SELECTOR, "figcaption.sc-1yvhotl-4.eStCAU h1").text
+
+                # 取得全部圖片網址
+                for img in imgSrc_elms:
+                    # 取得元素中的連結網址
+                    imgLink = img.get_attribute("href")
+                    # 將每張圖片的原始網址放進 listMulti_imgSrc 中
+                    listMulti_imgSrc.append(imgLink)
+
+                    # 將圖片超連結替換為有效的格式
+                    imgLink_useful = imgLink.replace("i.pximg.net", "i.pixiv.cat")
+                    # 將每張圖片的有效網址放進 listMulti_imgSrc_useful 中
+                    listMulti_imgSrc_useful.append(imgLink_useful)
+                
+                # 取得每張圖片的解析度字串
+                for i in listMulti_imgSrc_useful:           
+                    # 跳轉到圖片網址 (為了獲取圖片網址標題)
+                    driver.get(i)
+
+                    # 擷取圖片網址 title 文字 (為了獲取原始圖片解析度)
+                    web_page_title = driver.title
+                    # 取得圖片解析度字串 (透過正規表達式從 title 取得)
+                    img_resolution = re.search(r"[0-9]+×[0-9]+", web_page_title)[0]    
+                    # 將解析度字串放進 listMulti_imgResolution 中
+                    listMulti_imgResolution.append(img_resolution)
+
+                listData.append({
+                    "web_url":url,
+                    "author":drawer_name,
+                    "img_name":img_name,
+                    "img_number":img_number,
+                    "img_resolution":listMulti_imgResolution,
+                    "img_use_url":listMulti_imgSrc_useful,
+                    "img_origin_url":listMulti_imgSrc
+                })
+        
+            print("圖片資料已加入")
 
 
 ## 將 list 存成 json
@@ -183,33 +297,58 @@ def download_img():
     # print(listResult)
     # print("=" * 50)
 
-    # 建立儲存圖片、影片的資料夾
-    folderPath = "pixiv_img"
+    # 建立儲存圖片、影片的資料夾 (已存在就不建立)
+    folderPath = "pixiv_img_login"
     if not os.path.exists(folderPath):
         os.makedirs(folderPath)
     
     # 批次下載圖檔、重新命名
     for i, dictObj in enumerate(listResult):
-        # 取得副檔名 (透過正規表達式從網址取得)
-        extension = re.search(r".jp?g|.png", dictObj["img_use_url"])[0]       
-        # 用數字作為 os 下載檔名 (os curl 檔名遇到某些日文、簡中會變 "_"，目前不知道原因)
-        oldFileName = str(i) + extension
-        
-        # 下載檔案 (注意 "" 一定要在裡面，不然無法下載)
-        os.system('curl "{}" -o ./{}/{}'.format(dictObj["img_use_url"], folderPath, oldFileName))
- 
-        # 將作者、圖片名稱取出來作為正式檔名 (後綴加上圖片解析度、副檔名)
-        newFileName = dictObj["author"] + "_" + dictObj["img_name"] + "_" + dictObj["img_resolution"] + extension
-        
-        # 將 os 下載檔名重新命名為正式檔名
-        oldName = os.path.join("./", folderPath, oldFileName)
-        newName = os.path.join("./", folderPath, newFileName)
-        os.rename(oldName, newName)
+        ## 先判斷字典中的圖片是否有多張
+        # 如 img_number == 1，代表這個字典只有一張圖片
+        if dictObj["img_number"] == 1:
+            # 取得副檔名 (透過正規表達式從網址取得)
+            extension = re.search(r".jp?g|.png", dictObj["img_use_url"])[0]       
+            # 用數字作為 os 下載檔名 (os curl 檔名遇到某些日文、簡中會變 "_"，目前不知道原因)
+            oldFileName = str(i) + extension
+            
+            # 下載檔案 (注意 "" 一定要在裡面，不然無法下載)
+            os.system('curl "{}" -o ./{}/{}'.format(dictObj["img_use_url"], folderPath, oldFileName))
 
-        # 印出訊息
-        print("檔案名稱: {}".format(newFileName))
-        print("下載連結: {}".format(dictObj["img_use_url"]))
-        print()
+            # 將作者、圖片名稱、解析度取出來作為正式檔名 (後綴加上副檔名)
+            newFileName = dictObj["author"] + "_" + dictObj["img_name"] + "_" + dictObj["img_resolution"]  + extension
+            # 將 os 下載檔名重新命名為正式檔名
+            oldName = os.path.join("./", folderPath, oldFileName)
+            newName = os.path.join("./", folderPath, newFileName)
+            os.rename(oldName, newName)
+
+            # 印出訊息
+            print("檔案名稱: {}".format(newFileName))
+            print("下載連結: {}".format(dictObj["img_use_url"]))
+            print()
+        
+        # 如 img_number 不是 1，代表這個字典至少有 2 張圖片，使用巢狀迴圈下載
+        else:
+            for j, listObj in enumerate(dictObj["img_use_url"]):
+                # 取得副檔名 (透過正規表達式從網址取得)
+                extension = re.search(r".jp?g|.png", listObj)[0]       
+                # 用數字作為 os 下載檔名 (os curl 檔名遇到某些日文、簡中會變 "_"，目前不知道原因)
+                oldFileName = str(j) + extension
+
+                # 下載檔案 (注意 "" 一定要在裡面，不然無法下載)
+                os.system('curl "{}" -o ./{}/{}'.format(listObj, folderPath, oldFileName))
+
+                # 將作者、圖片名稱取出來作為正式檔名，圖名後面加上「第幾張圖片」數字，最後接圖片解析度 (後綴加上副檔名)
+                newFileName = dictObj["author"] + "_" + dictObj["img_name"] + "-"+ str(j + 1) + "_" + dictObj["img_resolution"][j]  + extension
+                # 將 os 下載檔名重新命名為正式檔名
+                oldName = os.path.join("./", folderPath, oldFileName)
+                newName = os.path.join("./", folderPath, newFileName)
+                os.rename(oldName, newName)
+
+                # 印出訊息
+                print("檔案名稱: {}".format(newFileName))
+                print("下載連結: {}".format(listObj))
+                print()
  
         
 
@@ -221,4 +360,5 @@ if __name__ == '__main__':
     img_url_name()
     savejson()
     download_img()
+
 
